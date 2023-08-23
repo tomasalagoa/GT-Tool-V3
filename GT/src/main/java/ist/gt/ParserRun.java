ae81222e9a0cb98d41544666daea7fcc881be051
DiogoLobato <43708723+DiogoLobato@users.noreply.github.com>
2023-08-23 22:14:44 +0100

    Cleans old (large) commented code

--- GT/src/main/java/ist/gt/ParserRun.java
+++ GT/src/main/java/ist/gt/ParserRun.java
@@ -12,19 +12,14 @@ public class ParserRun {
             new FuncDefinition("executeQuery")), List.of(
                 new FuncDefinition("sanitize")));
     private final static Settings settings = new Settings();
-    private final static String DirectoryPath = "C:\\Users\\diogo\\Desktop\\MEIC-T\\2Ano\\Tese\\MasterDissertation\\GT-Tool-V2\\GT\\src\\test\\java\\javaLang";
+    private final static String DirectoryPath = "src/test/java/javaLang";
 
     public static void main(String[] args) throws Exception {
-//        AstConverter.analyseAndProduceReport("src/main/resources/example/AnotherDir/LoginServlet.java", "src/main/resources/settings.json");
-//        AstConverter.analyseAndProduceReport("GT\\src\\main\\resources\\settings.json");
-//        AstConverter.analyseAndProduceReport("src/main/resources/example/example1.php", "src/main/resources/settingsphp.json");
         settings.setFileExtension("java");
         spec.setFileName("Experiments.java");
         spec.getFunction().setType(spec.getFileName().replace(".java", ""));
         settings.setSpecification(spec);
-        AstConverter.analyse(DirectoryPath + "\\experiments", settings);
+        AstConverter.analyse(DirectoryPath + "/experiments", settings);
         System.out.println("Success");
-
-//        AstConverter.convertDirectory("src/main/resources/example", "java");
     }
 }
